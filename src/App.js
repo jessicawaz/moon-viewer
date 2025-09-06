a248ae412c252946e55f705ee9cd81067973255a
jessicawaz <jess05@gmail.com>
2025-09-06 11:31:21 -0400

    Add icon, fix styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import SunCalc from "suncalc";
 import { CiLocationArrow1 } from "react-icons/ci";
+import { FaMoon } from "react-icons/fa";
 
 import "./App.css";
 
@@ -129,137 +130,115 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <div
-        style={{
-          position: "absolute",
-          top: 50,
-          left: "50%",
-          transform: "translateX(-50%)",
-          fontSize: "2rem",
-          zIndex: 1000,
-        }}
-      >
-        lat: {userLocation.lat} <br />
-        long: {userLocation.long}
+    <div className={"App"}>
+      <div className={"MoonLogo"}>
+        <FaMoon size={60} />
+        <span>Madison's Moon Viewer</span>
       </div>
 
-      <button
-        style={{
-          position: "absolute",
-          top: "60%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          fontSize: "1rem",
-          padding: "1rem 2rem",
-          zIndex: 1000,
-          background: "#222",
-          color: "#fff",
-          border: "none",
-          borderRadius: 8,
-          boxShadow: "0 2px 8px #0008",
-        }}
-        onClick={getUserLocation}
-      >
-        Request Location
-      </button>
-
-      <div
-        style={{
-          position: "absolute",
-          top: 90,
-          left: "50%",
-          transform: "translateX(-50%)",
-          fontSize: "1rem",
-          zIndex: 1000,
-        }}
-      >
-        {moonVisibilityTimes.rise && moonVisibilityTimes.set
-          ? `The Moon will be visible from
+      <div className={"PageWrapper"}>
+        {userLocation?.lat && userLocation?.long ? (
+          <div
+            style={{
+              fontSize: "1.5rem",
+              marginBottom: '16px',
+              zIndex: 1000,
+            }}
+          >
+            {moonVisibilityTimes.rise && moonVisibilityTimes.set
+              ? `The Moon will be visible from
         ${moonVisibilityTimes.rise?.toLocaleString(dateLocales, dateOptions)} to
         ${moonVisibilityTimes.set?.toLocaleString(
           dateLocales,
           dateOptions
         )} (Eastern)`
-          : moonVisibilityTimes.rise && !moonVisibilityTimes.set
-          ? `The Moon will be visible starting at ${moonVisibilityTimes.rise?.toLocaleString(
-              dateLocales,
-              dateOptions
-            )} (Eastern)`
-          : !moonVisibilityTimes.rise && moonVisibilityTimes.set
-          ? `The Moon is visible now through ${moonVisibilityTimes.set?.toLocaleString(
-              dateLocales,
-              dateOptions
-            )} (Eastern)`
-          : "The moon will not be visible tonight."}
-      </div>
+              : moonVisibilityTimes.rise && !moonVisibilityTimes.set
+              ? `The Moon will be visible starting at ${moonVisibilityTimes.rise?.toLocaleString(
+                  dateLocales,
+                  dateOptions
+                )} (Eastern)`
+              : !moonVisibilityTimes.rise && moonVisibilityTimes.set
+              ? `The Moon is visible now through ${moonVisibilityTimes.set?.toLocaleString(
+                  dateLocales,
+                  dateOptions
+                )} (Eastern)`
+              : "The moon will not be visible tonight."}
+          </div>
+        ) : (
+          <button
+            style={{
+              fontSize: "1rem",
+              padding: "1rem 2rem",
+              zIndex: 1000,
+              background: "#222",
+              color: "#fff",
+              border: "none",
+              borderRadius: 8,
+              boxShadow: "0 2px 8px #0008",
+            }}
+            onClick={getUserLocation}
+          >
+            Request Location
+          </button>
+        )}
 
-      {errorMessage && (
+        {errorMessage && (
+          <div
+            style={{
+              fontSize: "1.5rem",
+              marginTop: 40,
+            }}
+          >
+            {errorMessage}
+          </div>
+        )}
+
+        {/* Display the feed from the user's camera */}
+        {cameraStarted ? (
+          <video
+            autoPlay
+            playsInline
+            ref={cameraRef}
+            style={{
+              position: "absolute",
+              width: "100dvw",
+              height: "100dvh",
+              objectFit: "cover",
+              zIndex: 0,
+              left: 0,
+            }}
+          />
+        ) : (
+          <button
+            style={{
+              fontSize: "1rem",
+              padding: "1rem 2rem",
+              zIndex: 1000,
+              background: "#222",
+              color: "#fff",
+              border: "none",
+              borderRadius: 8,
+              boxShadow: "0 2px 8px #0008",
+              marginTop: 8,
+            }}
+            onClick={() => setCameraStarted(true)}
+          >
+            Request Camera
+          </button>
+        )}
+
+        {/* Points towards the moon direction */}
         <div
+          id="pointer"
           style={{
-            position: "absolute",
-            bottom: "20px",
-            fontSize: "1.5rem",
-            left: "50%",
-            transform: "translateX(-50%)",
-            width: "100dvw",
+            background: "none",
+            pointerEvents: "none",
+            zIndex: 10,
+            marginTop: 40,
           }}
         >
-          {errorMessage}
+          <CiLocationArrow1 size={60} />
         </div>
-      )}
-
-      {/* Display the feed from the user's camera */}
-      {cameraStarted ? (
-        <video
-          autoPlay
-          playsInline
-          ref={cameraRef}
-          style={{
-            position: "absolute",
-            width: "100dvw",
-            height: "100dvh",
-            objectFit: "cover",
-            zIndex: 0,
-            left: 0,
-          }}
-        />
-      ) : (
-        <button
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            fontSize: "1rem",
-            padding: "1rem 2rem",
-            zIndex: 1000,
-            background: "#222",
-            color: "#fff",
-            border: "none",
-            borderRadius: 8,
-            boxShadow: "0 2px 8px #0008",
-          }}
-          onClick={() => setCameraStarted(true)}
-        >
-          Request Camera
-        </button>
-      )}
-
-      {/* Points towards the moon direction */}
-      <div
-        id="pointer"
-        style={{
-          position: "absolute",
-          bottom: "50%",
-          left: "50%",
-          background: "none",
-          pointerEvents: "none",
-          zIndex: 10,
-          transform: `translate(-50%,-50%) rotate(${rotation}deg)`,
-        }}
-      >
-        <CiLocationArrow1 size={60} />
       </div>
     </div>
   );
